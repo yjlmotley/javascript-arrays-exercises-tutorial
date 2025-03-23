5d22dfcfc7e6c1ab6ed853165701d17d6e368389
Yeju Lee Motley <yejuleemotley@gmail.com>
2025-03-23 10:17:25 -0700

    finished upto exercise 19

--- exercises/17-The-for-loop/app.js
+++ exercises/17-The-for-loop/app.js
@@ -1,3 +1,9 @@
 let myArray = [2323, 4344, 2325, 324413, 21234, 24531, 2123, 42234, 544, 456, 345, 42, 5445, 23, 5656, 423];
 
 // Your code here
+let elementSum = 0;
+for (let element of myArray) {
+    elementSum += element;
+    // console.log("sum: " + elementSum);
+}
+console.log(elementSum / myArray.length);
